e6063ebf525651a6cee03930ee38f82264587429
Ubuntu <ubuntu@ip-172-31-20-22.ec2.internal>
2026-07-30 10:59:08 +0000

    Added S3 bucket

--- main.tf
+++ main.tf
@@ -5,3 +5,12 @@ terraform {
 provider "aws" {
   region = "us-east-1c"
 }
+
+resource "aws_s3_bucket" "terraform_demo" {
+  bucket = "uday-terraform-demo-2026"
+
+  tags = {
+    Name = "Terraform Demo Bucket"
+    Environment = "Lab"
+  }
+}
